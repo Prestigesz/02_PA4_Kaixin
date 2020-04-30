d81d32a1978f22a8980c08e7886db32b4c1dd352
SujitKumarKuwar <sujitkuwar3@gmail.com>
2020-04-30 15:12:33 +0800

    Updated MainScene by sujit

---
+++ 02_kaixin_PA4/Assets/Script/ChangeScenes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+    void MyLoadingFunction()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time>=1)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
